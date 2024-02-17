a458fa3836d26045bc29436d9a027e771a67799d
NeroBirdy <nirvanav.39@gmail.com>
2024-02-17 15:04:52 +0500

    залил default laravel проект

--- SERV_LABA1/routes/web.php
+++ SERV_LABA1/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\InfoController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,9 +16,3 @@ use App\Http\Controllers\InfoController;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/info/server', [InfoController::class, 'serverInfo']);
-
-Route::get('/info/client', [InfoController::class, 'clientInfo']);
-
-Route::get('/info/database', [InfoController::class, 'databaseInfo']);
